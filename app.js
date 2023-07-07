6251e99c80266b136d86f3831991707633335081
andikastp <andikapangestu79@gmailcom>
2023-07-07 12:05:11 +0700

    added backend, connect to mongodb, create get all post function,get single post with it own page, search post by title

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ require("dotenv").config();
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
 
+const connectDB = require('./server/config/database');
+
 const app = express();
 const PORT = 3000 || process.env.PORT;
+
+//connect to db
+connectDB();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static("public"));
 
