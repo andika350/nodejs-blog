b81d42bb31cd88db68a2c6e269df418d83d23bee
andikastp <andikapangestu79@gmailcom>
2023-07-07 17:33:53 +0700

    a color change in css, create add new post page and route

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Post = require("../models/Post");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const adminLayout = '../views/layouts/admin';
+const adminLayout = "../views/layouts/admin";
 const jwtSecret = process.env.JWT_SECRET;
 
 //Check login
@@ -23,9 +23,7 @@ const authMiddleware = (req, res, next) => {
       res.status(401).json({ message: "Unauthorized" });
     }
   }
-}
-
-
+};
 
 //GET Admin home, no pagination
 router.get("/admin", async (req, res) => {
@@ -35,7 +33,6 @@ router.get("/admin", async (req, res) => {
       description: "Simple Blog created with Nodejs, Express & Mongodb",
     };
 
-    
     res.render("admin/index", { locals, layout: adminLayout });
   } catch (error) {
     console.log(error);
@@ -61,47 +58,90 @@ router.post("/admin", async (req, res) => {
     }
 
     const token = jwt.sign({ userId: user._id }, jwtSecret);
-    res.cookie('token', token, { httpOnly: true });
-    res.redirect('/dashboard');
-    
+    res.cookie("token", token, { httpOnly: true });
+    res.redirect("/dashboard");
   } catch (error) {
     console.log(error);
   }
 });
 
-//Redirect to admin dashboard
+//GET admin dashboard
+router.get("/dashboard", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Dashboard",
+      description: "Simple Blog created with Nodejs, Express & Mongodb",
+    };
+    const data = await Post.find();
+    res.render("admin/dashboard", {
+      locals,
+      data,
+      layout: adminLayout
+    });
+  } catch (error) {console.log(error)}
+});
 
-router.get("/dashboard",authMiddleware, async (req, res) => {
-  res.render('admin/dashboard')
+//GET New Post
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Add Post",
+      description: "Simple Blog created with Nodejs, Express & Mongodb",
+    };
+    const data = await Post.find();
+    res.render("admin/add-post", {
+      locals,
+      layout: adminLayout,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//POST New Post
+router.post("/add-post", authMiddleware, async (req, res) => {
+  try {
+    
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body
+      });
+
+      await Post.create(newPost);
+      res.redirect('/dashboard')
+
+    } catch (error) {
+      console.log(error)
+    }
+    
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 //POST admin register
 
 router.post("/register", async (req, res) => {
   try {
-   
     const { username, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
       const user = await User.create({ username, password: hashedPassword });
-      res.status(201).json({ message: 'User created', user });
+      res.status(201).json({ message: "User created", user });
     } catch (error) {
       if (error.code === 11000) {
-        res.status(409).json({message:'Failed to create user'})
+        res.status(409).json({ message: "Failed to create user" });
       }
-      res.status(500).json({message:"Internal server error"})
+      res.status(500).json({ message: "Internal server error" });
     }
-
-    
   } catch (error) {
     console.log(error);
   }
 });
 
 module.exports = router;
-
-
 
 //POST admin check login
 // router.post("/admin", async (req, res) => {
